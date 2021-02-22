d3b120ca586c053643aec72b8600f0a1b809238b
Tord Johan Espe <tordjohanespe@gmail.com>
2021-02-22 11:20:51 +0100

    Rename and create tile class

---
+++ project/src/main/java/minesweeper/model/Tile.java
@@ -0,0 +1,49 @@
+package minesweeper.model;
+
+public class Tile {
+    private boolean hasBomb;
+    private boolean isOpened;
+    private boolean isFlagged;
+
+    public Tile(boolean bomb) {
+        this.hasBomb = bomb;
+        this.isOpened = false;
+        this.isFlagged = false;
+    }
+
+    public void toggleFlag() {
+        this.isFlagged = !this.isFlagged;
+    }
+
+    public void open() throws IllegalStateException {
+        if (this.isOpened)
+            throw new IllegalStateException("This field has already been opened");
+        this.isOpened = true;
+    }
+
+    public boolean getHasBomb() {
+        return this.hasBomb;
+    }
+
+    public boolean getIsFlagged() {
+        return this.isFlagged;
+    }
+
+    public boolean getIsOpened() {
+        return this.isOpened;
+    }
+
+    public String toString(int adjacentBombs) {
+        if (!isOpened) {
+            if (isFlagged)
+                return "⛳";
+            return "⬜️";
+        }
+        if (hasBomb)
+            return "💣";
+        if (adjacentBombs > 0)
+            return Integer.toString(adjacentBombs);
+        return " ";
+    }
+
+}
