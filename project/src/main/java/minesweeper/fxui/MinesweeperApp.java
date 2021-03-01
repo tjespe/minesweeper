39fdce377307c70418bafa244a3c1f9ef6c0f8f5
GRUDE <kristoffenbg@gmail.com>
2021-03-01 17:10:38 +0100

    create app class to run application

--- project/src/main/java/minesweeper/fxui/MinesweeperApp.java
+++ project/src/main/java/minesweeper/fxui/MinesweeperApp.java
@@ -1,5 +1,21 @@
 package minesweeper.fxui;
 
-public class MinesweeperApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class MinesweeperApp extends Application{
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("Minesweeper.fxml"));
+		stage.setTitle("Minesweeper");
+		stage.setScene(new Scene(parent));
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(MinesweeperApp.class, args);
+	}
 }
