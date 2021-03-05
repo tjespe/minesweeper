6dc302dd149d827ad3d7ce501f76ac1d16f39fe2
GRUDE <kristoffenbg@gmail.com>
2021-03-05 20:27:33 +0100

    add levels to dropDown in controller

--- project/src/main/java/minesweeper/fxui/MinesweeperController.java
+++ project/src/main/java/minesweeper/fxui/MinesweeperController.java
@@ -1,5 +1,15 @@
 package minesweeper.fxui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class MinesweeperController {
 
+	@FXML
+	private ChoiceBox<String> dropDown;
+	
+	public void initialize() {
+		dropDown.getItems().addAll("Easy", "Normal", "Hard");
+		dropDown.setValue("Normal");
+	}
 }
