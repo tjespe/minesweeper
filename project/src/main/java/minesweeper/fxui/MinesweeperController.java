c4adff589c198e7f5b09466db5849a7a277ab2ff
GRUDE <kristoffenbg@gmail.com>
2021-04-03 17:10:01 +0200

    add style to fields

--- project/src/main/java/minesweeper/fxui/MinesweeperController.java
+++ project/src/main/java/minesweeper/fxui/MinesweeperController.java
@@ -1,61 +1,109 @@
 package minesweeper.fxui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Labeled;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import minesweeper.model.Board;
+import minesweeper.model.Field;
 
 
 public class MinesweeperController {
 
 	@FXML private ChoiceBox<String> dropDown;
 	@FXML private GridPane boardParent;
+	private Board board;
+	
 	
 	
 	public void initialize() {
 		dropDown.getItems().addAll("Easy", "Normal", "Hard");
 		dropDown.setValue("Normal");
 		dropDown.setOnAction(event -> {changeDifficultLevel();});
+		board = new Board(14,18,40);
+		drawBoard();
 		
 		
+	}
+	
+	private void drawBoard() {
 		
-		Board board = new Board(14,18,40);
 		
 		//create board tiles 
 		boardParent.getChildren().clear();
 		for (int col = 0; col < 18; col++) {
 			for (int row = 0; row < 14; row++) {
-				createBoardTile(col, row);
+				createBoardField(col, row);
 				//((Labeled) boardParent.getChildren().get(col*14+row)).setText("1");
-				boardParent.getChildren().get(col*14+row);
-				//board.fields.get();
+				//boardParent.getChildren().get(col*14+row);
 				
+				//board.getFieldStatus(col, row);
 			}
 		}
+		
+		//TODO ADD STYLE
+		//check status of game 
+		if (board.getStatus() == 'w') {
+			System.out.println("You won");
+		} else if (board.getStatus() == 'l') {
+			System.out.println("You lost");
+		}
+		
 	}
-	
-	//TODO create method with initial game state that is called in initialize
 
-	private void createBoardTile(int row, int col) {
-		Button field = new Button();
-        //Pane field = new Pane();
+	private void createBoardField(int row, int col) {
+		//Button field = new Button();
+        Pane field = new Pane();
 		//button.setText(" ");
         //button.setMaxSize(40, 40);
-		field.setTranslateX(row);
+        field.setTranslateX(row);
 		field.setTranslateY(col);
 		field.setPrefWidth(30);
 		field.setPrefHeight(30);
+		field.setStyle("-fx-background-color: " + getFieldStyle(board.getFieldStatus(row,col))); //#1db121
         boardParent.add(field, row+1, col+1);
+		
+	}
+	
+	@FXML
+	public void changeDifficultLevel() {
+		System.out.println(dropDown.getValue());
+	}
+	
+	@FXML
+	public void boardMouseClickedHandler(MouseEvent e) {
+		Node boardField = e.getPickResult().getIntersectedNode();
+		int x = GridPane.getColumnIndex(boardField).intValue();
+		int y = GridPane.getRowIndex(boardField).intValue();
+		System.out.println(x+","+y);
+		if (e.getButton() == MouseButton.PRIMARY) {
+			System.out.println("Left button clicked");
+			board.openField(x-1,y-1);
+			drawBoard();
+		} else if (e.getButton() == MouseButton.SECONDARY) {
+			System.out.println("Right button clicked");
+			//TODO toggle flag 
+		}
 		
 	}
 	
 	
 	
-	@FXML
-	public void changeDifficultLevel() {
-		System.out.println(dropDown.getValue());
+	private String getFieldStyle(char fieldStatus) {
+		if (fieldStatus == 'u') {
+			return "#1db121";
+		} else if (fieldStatus == 'f') {
+			return "#FF0000";
+		} else if (fieldStatus == 'o') {
+			return "#ffffff";
+		} else if (fieldStatus == 'b') {
+			return "#000000";
+		}
+		return null;
+		
 	}
 	
 }
