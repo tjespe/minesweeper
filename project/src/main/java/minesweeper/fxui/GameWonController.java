7e3656d1fb9dfcee5439a94ed2d7069b61829775
Tord Johan Espe <tordjohanespe@gmail.com>
2021-04-21 22:11:59 +0200

    Complete modal for saving new highscores

--- project/src/main/java/minesweeper/fxui/GameWonController.java
+++ project/src/main/java/minesweeper/fxui/GameWonController.java
@@ -1,14 +1,18 @@
 package minesweeper.fxui;
 
-import java.io.IOException;
-
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import minesweeper.model.HighscoreList;
-import minesweeper.model.ReadAndWriteHighscoreList;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import minesweeper.model.Score;
 
 public class GameWonController {
+    @FXML
+    private AnchorPane rootPane;
     @FXML
     private TextField name;
 
@@ -20,8 +24,21 @@ public class GameWonController {
 
     @FXML
     public void submitClickHandler(MouseEvent event) {
-        System.out.println(name.getText());
-
+        if (parentController == null) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Unexpected error!");
+            alert.setHeaderText("Unexpected error!");
+            alert.showAndWait();
+            return;
+        }
+        String time = parentController.getCurrentTime();
+        Score score = new Score(name.getText(), time);
+        parentController.highscores.addScore(score);
+        Scene scene = rootPane.getScene();
+        if (scene != null) {
+            Stage stage = (Stage) scene.getWindow();
+            stage.close();
+        }
     }
 
 }
