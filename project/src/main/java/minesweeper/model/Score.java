8fb3d8804cf086919578d5d5e852bce191af2ec3
Tord Johan Espe <tordjohanespe@gmail.com>
2021-04-21 22:28:35 +0200

    Add DifficultyLevel to Score format

--- project/src/main/java/minesweeper/model/Score.java
+++ project/src/main/java/minesweeper/model/Score.java
@@ -3,26 +3,29 @@ package minesweeper.model;
 public class Score implements Comparable<Score> {
 	private String name;
 	private String time;
-	
-	public Score(String name, String time) {
+	private DifficultyLevel level;
+
+	public Score(String name, String time, DifficultyLevel level) {
 		this.name = name;
 		this.time = time;
+		this.level = level;
 	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getTime() {
 		return time;
 	}
-	
+
 	@Override
 	public int compareTo(Score score) {
 		return this.getTime().compareTo(score.getTime());
 	}
-	
+
 	@Override
 	public String toString() {
-		return getName() + "," + getTime();
+		return getName() + "," + getTime() + "," + (this.level != null ? this.level.toString() : "Unknown");
 	}
 }
