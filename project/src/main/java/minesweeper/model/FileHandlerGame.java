d4bb1b008aa77e05ac577d82d62e02e292c2a9ad
KGrude <77727766+KGrude@users.noreply.github.com>
2021-04-21 13:19:36 +0200

    add skeleton for write and read to file (#19)
    Co-authored-by: GRUDE <kristoffenbg@gmail.com>

---
+++ project/src/main/java/minesweeper/model/FileHandlerGame.java
@@ -0,0 +1,17 @@
+package minesweeper.model;
+
+public class FileHandlerGame implements IReadAndWriteFile {
+
+	@Override
+	public void readFromFile() {
+		// TODO create method for reading game state to file
+		
+	}
+
+	@Override
+	public void writeToFile() {
+		// create method for writing game state to file
+		
+	}
+
+}
