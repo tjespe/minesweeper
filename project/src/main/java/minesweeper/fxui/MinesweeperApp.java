6c252f63d53fc051cac104369645d75d7da9aeb3
Tord Johan Espe <tordjohanespe@gmail.com>
2021-04-27 19:12:37 +0200

    Quit process when window is closed

--- project/src/main/java/minesweeper/fxui/MinesweeperApp.java
+++ project/src/main/java/minesweeper/fxui/MinesweeperApp.java
@@ -1,10 +1,13 @@
 package minesweeper.fxui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 
 public class MinesweeperApp extends Application {
 	@Override
@@ -14,6 +17,13 @@ public class MinesweeperApp extends Application {
 		stage.setScene(new Scene(parent));
 		stage.show();
 		stage.setResizable(false);
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent t) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
 	}
 
 	public static void main(String[] args) {
